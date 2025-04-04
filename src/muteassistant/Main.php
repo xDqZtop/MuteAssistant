cc5b2859da9ef794acbe1a360f16405b99f96f6a
xDqZtop <xdqztop@gmail.com>
2025-04-04 19:34:17 +0330

    convert pm5

--- src/muteassistant/Main.php
+++ src/muteassistant/Main.php
@@ -1,108 +1,128 @@
 <?php
-
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace muteassistant;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as TF;
 use muteassistant\form\Form;
+use SQLite3;
 
 class Main extends PluginBase implements Listener {
 
-  public const INFO = TF::BOLD . TF::YELLOW . "INFO " . TF::RESET . TF::YELLOW;
-  public const ERROR = TF::BOLD . TF::RED . "ERROR " . TF::RESET . TF::RED;
-  public $db;
-  public $target = [];
-  public $form;
+    public const string INFO = TF::BOLD . TF::YELLOW . "INFO " . TF::RESET . TF::YELLOW;
+    public const string ERROR = TF::BOLD . TF::RED . "ERROR " . TF::RESET . TF::RED;
+    public SQLite3 $db;
+    public array $target = [];
+    public Form $form;
 
-  public function onLoad(){
-    $this->getLogger()->info(self::INFO . "Plugin is loading...");
-    @mkdir($this->getDataFolder());
-    $this->db = new \SQLite3($this->getDataFolder() . "MuteListData.db");
-    $this->db->exec("CREATE TABLE IF NOT EXISTS mutelist(player TEXT PRIMARY KEY, time INT, reason TEXT, staff TEXT);");
-  }
+    public function onLoad(): void {
+        $this->getLogger()->info(self::INFO . "Plugin is loading...");
+        @mkdir($this->getDataFolder());
+        $this->db = new SQLite3($this->getDataFolder() . "MuteListData.db");
+        $this->db->exec("CREATE TABLE IF NOT EXISTS mutelist(player TEXT PRIMARY KEY, time INT, reason TEXT, staff TEXT);");
+    }
 
-  public function onEnable(){
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->form = new Form($this);
-  }
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->form = new Form($this);
+    }
 
-  public function onDisable(){
-    $this->getLogger()->info(self::INFO . "Plugin disabled.");
-  }
+    public function onDisable(): void {
+        $this->getLogger()->info(self::INFO . "Plugin disabled.");
+    }
 
-  public function onPlayerChat(PlayerChatEvent $ev){
-    $player = $ev->getPlayer();
-    $pn = $player->getName();
-    $db = $this->db->query("SELECT * FROM mutelist WHERE player = '$pn';");
-    $data = $db->fetchArray(SQLITE3_ASSOC);
-    if(!empty($data)){
-      if($data["time"] == -1){
-        $ev->setCancelled();
-        $player->sendMessage(self::INFO . "You have been muted premently. If we made a mistake contact us to review.");
-      } else {
-        if($data["time"] > time()){
-          $ev->setCancelled();
-          $rt = $data["time"] - time();
-          $rtd = floor($rt / 86400);
-          $rths = $rt % 86400;
-          $rth = floor($rths / 3600);
-          $rtms = $rths % 3600;
-          $rtm = floor($rtms / 60);
-          $rtss = $rtms % 60;
-          $rts = ceil($rtss);
-          $player->sendMessage(self::INFO . "You have been muted for " . TF::LIGHT_PURPLE . $rtd . " days, " . $rth . " hours, " . $rtm . " minutes, " . $rts . " seconds" . TF::YELLOW . ". Please avoid chatting!");
-        } else {
-          $this->db->query("DELETE FROM mutelist WHERE player = '$pn';");
+    public function onPlayerChat(PlayerChatEvent $ev): void {
+        $player = $ev->getPlayer();
+        $pn = $player->getName();
+        $db = $this->db->query("SELECT * FROM mutelist WHERE player = '$pn';");
+        $data = $db->fetchArray(SQLITE3_ASSOC);
+
+        if(empty($data)) return;
+
+        if($data["time"] == -1){
+            $ev->cancel();
+            $player->sendMessage(self::INFO . "You have been muted permanently. If we made a mistake contact us to review.");
+            return;
         }
-      }
-    }
-  }
 
-  public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
-    if($sender instanceof Player){
-      if($sender->hasPermission("muteassistant.command.mute")){
+        if($data["time"] > time()){
+            $ev->cancel();
+            $this->sendTimeLeftMessage($player, $data["time"]);
+        } else {
+            $this->db->query("DELETE FROM mutelist WHERE player = '$pn';");
+        }
+    }
+
+    private function sendTimeLeftMessage(Player $player, int $muteTime): void {
+        $rt = $muteTime - time();
+        $rtd = floor($rt / 86400);
+        $rths = $rt % 86400;
+        $rth = floor($rths / 3600);
+        $rtms = $rths % 3600;
+        $rtm = floor($rtms / 60);
+        $rtss = $rtms % 60;
+        $rts = ceil($rtss);
+
+        $player->sendMessage(self::INFO . "You have been muted for " . TF::LIGHT_PURPLE .
+            "$rtd days, $rth hours, $rtm minutes, $rts seconds" . TF::YELLOW . ". Please avoid chatting!");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if(!$sender instanceof Player){
+            $sender->sendMessage(self::ERROR . "You can use this command only in-game");
+            return true;
+        }
+
+        if(!$sender->hasPermission("muteassistant.command.mute")){
+            $sender->sendMessage(self::ERROR . "You don't have permission to use this command");
+            return true;
+        }
+
         $this->form->openMainForm($sender);
-      } else {
-        $sender->sendMessage(Main::ERROR . "You don't have permission to use this command");
-      }
-    } else {
-      $sender->sendMessage(Main::ERROR . "You can use this command only in-game");
+        return true;
     }
-    return true;
-  }
 
-  public function mute(Player $staff, Player $target, string $reason, $day = 0, $hour = 0, $minute = 0){
-    if($day == "N/A"){
-      $db = $this->db->prepare("INSERT OR REPLACE INTO mutelist (player, time, reason, staff) VALUES (:player, :time, :reason, :staff);");
-      $db->bindValue(":player", $target->getName());
-      $db->bindValue(":time", -1);
-      $db->bindValue(":reason", $reason);
-      $db->bindValue(":staff", $staff->getName());
-      $db->execute();
-      $staff->sendMessage(self::INFO . "Player " . TF::LIGHT_PURPLE . $target->getName() . TF::YELLOW . " muted for " . TF::RED . "EVER");
-      $target->sendMessage(self::INFO . "You have been muted from chat for " . TF::RED . "EVER" . TF::YELLOW . "! It was a mistake? Contact us to review.");
-      return true;
+    public function mute(Player $staff, Player $target, string $reason, $day = 0, $hour = 0, $minute = 0): bool {
+        if($day === "N/A"){
+            $this->executePermanentMute($staff, $target, $reason);
+            return true;
+        }
+
+        $this->executeTemporaryMute($staff, $target, $reason, $day, $hour, $minute);
+        return true;
     }
-    $d = $day * 86400;
-    $h = $hour * 3600;
-    $m = $minute * 60;
-    $mt = time() + $d + $h + $m;
-    $db = $this->db->prepare("INSERT OR REPLACE INTO mutelist (player, time, reason, staff) VALUES (:player, :time, :reason, :staff);");
-    $db->bindValue(":player", $target->getName());
-    $db->bindValue(":time", $mt);
-    $db->bindValue(":reason", $reason);
-    $db->bindValue(":staff", $staff->getName());
-    $db->execute();
-    $staff->sendMessage(self::INFO . "Player " . TF::LIGHT_PURPLE . $target->getName() . TF::YELLOW . " muted for " . TF::LIGHT_PURPLE . $day . " days, " . $hour . " hours, " . $minute . " minutes");
-    $target->sendMessage(self::INFO . "You have been muted from chat for " . TF::LIGHT_PURPLE . $day . " days, " . $hour . " hours, " . $minute . " minutes" . TF::YELLOW . ". Please avoid chatting!");
-  }
 
+    private function executePermanentMute(Player $staff, Player $target, string $reason): void {
+        $db = $this->db->prepare("INSERT OR REPLACE INTO mutelist (player, time, reason, staff) VALUES (:player, :time, :reason, :staff);");
+        $db->bindValue(":player", $target->getName());
+        $db->bindValue(":time", -1);
+        $db->bindValue(":reason", $reason);
+        $db->bindValue(":staff", $staff->getName());
+        $db->execute();
+
+        $staff->sendMessage(self::INFO . "Player " . TF::LIGHT_PURPLE . $target->getName() . TF::YELLOW . " muted for " . TF::RED . "EVER");
+        $target->sendMessage(self::INFO . "You have been muted from chat for " . TF::RED . "EVER" . TF::YELLOW . "! It was a mistake? Contact us to review.");
+    }
+
+    private function executeTemporaryMute(Player $staff, Player $target, string $reason, int $day, int $hour, int $minute): void {
+        $mt = time() + ($day * 86400) + ($hour * 3600) + ($minute * 60);
+
+        $db = $this->db->prepare("INSERT OR REPLACE INTO mutelist (player, time, reason, staff) VALUES (:player, :time, :reason, :staff);");
+        $db->bindValue(":player", $target->getName());
+        $db->bindValue(":time", $mt);
+        $db->bindValue(":reason", $reason);
+        $db->bindValue(":staff", $staff->getName());
+        $db->execute();
+
+        $staff->sendMessage(self::INFO . "Player " . TF::LIGHT_PURPLE . $target->getName() . TF::YELLOW .
+            " muted for " . TF::LIGHT_PURPLE . "$day days, $hour hours, $minute minutes");
+        $target->sendMessage(self::INFO . "You have been muted from chat for " . TF::LIGHT_PURPLE .
+            "$day days, $hour hours, $minute minutes" . TF::YELLOW . ". Please avoid chatting!");
+    }
 }
--- src/muteassistant/form/Form.php
+++ src/muteassistant/form/Form.php
@@ -1,10 +1,10 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace muteassistant\form;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\form\Form as PocketmineForm;
 use muteassistant\Main;
@@ -13,220 +13,209 @@ use muteassistant\lib\jojoe77777\FormAPI\CustomForm as CF;
 
 class Form implements PocketmineForm {
 
-  public $plugin;
-  public $data = [];
-  public $l = [];
-  public $target = [];
+    public Main $plugin;
+    public array $data = [];
+    public array $l = [];
+    public array $target = [];
 
-  public function __construct(Main $plugin){
-    $this->plugin = $plugin;
-  }
-
-  public function handleResponse(Player $player, $data) : void {
-    
-  }
-
-  public function jsonSerialize(){
-    return $this->data;
-  }
-
-  public function resort(array $array, bool $type = false) : array {
-    $a = $array;
-    arsort($a);
-    if($type) return array_reverse($a);
-    return array_reverse(array_values($a));
-  }
-
-  public function openErrorForm(Player $player, string $title = "Error", string $content = "Something went wrong!", array $buttons = []){
-    $this->data = [];
-    $this->data["type"] = "form";
-    $this->data["title"] = $title;
-    $this->data["content"] = $content . str_repeat("\n", 15);
-    $this->data["buttons"] = $buttons;
-    $player->sendForm($this);
-  }
-
-  public function openMainForm(Player $player){
-    $form = new SF(function (Player $player, $data = null){
-      $result = $data;
-      if($result === null) return true;
-      switch($result){
-        case 0:
-          $this->openMuteForm($player);
-        break;
-        case 1:
-          $this->openMuteByNameForm($player);
-        break;
-        case 2:
-          $this->openMuteListForm($player);
-        break;
-      }
-    });
-    $form->setTitle("Mute Assistant Menu");
-    $form->setContent("Choose an option:");
-    $form->addButton("Mute");
-    $form->addButton("Mute By Name");
-    //$form->addButton("Mute By Stick");
-    $form->addButton("Mute List");
-    //$form->addButton("Settings");
-    $form->sendToPlayer($player);
-    return $form;
-  }
-
-  public function openMuteForm(Player $player){
-      $this->l = [];
-    foreach($this->plugin->getServer()->getOnlinePlayers() as $p){
-      $this->l[] = $p->getName();
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
     }
-    $form = new CF(function (Player $player, array $data = null){
-      if($data == null) return true;
-      $a = $this->resort($this->l);
-      $target = $this->plugin->getServer()->getPlayerExact($a[$data[1]]);
-      if($a[$data[1]] === $player->getName()){
-        $player->sendMessage(Main::ERROR . "You can not mute your self");
-      }
-      if($target instanceof Player){
-        if($target->hasPermission("muteassistant.command.mute") or $target->isOp()){
-          $player->sendMessage(Main::ERROR . "You can not mute server staff");
-        } else {
-          if($data[3]){
-            $this->plugin->mute($player, $target, $data[2], $data[4], $data[5], $data[6]);
-          } else {
-            $this->plugin->mute($player, $target, $data[2], "N/A", "N/A", "N/A");
-          }
-        }
-      } else {
-        $player->sendMessage(TF::RED . "Player not found!");
-      }
-      $this->l = [];
-    });
-    $form->setTitle("Mute Form");
-    $form->addLabel("Fill the form and submit to mute your target player:\n\n");
-    $form->addDropdown("Target:", $this->resort($this->l));
-    $form->addInput("Reason:", "type here...");
-    $form->addToggle("Prement:", false);
-    $form->addSlider("Day(s)", 0, 30, 1, 0);
-    $form->addSlider("Hour(s)", 0, 24, 1, 0);
-    $form->addSlider("Minutes(s)", 0, 60, 2, 2);
-    $form->sendToPlayer($player);
-    return $form;
-  }
 
-  public function openMuteByNameForm(Player $player, bool $type = false){
-    $form = new CF(function (Player $player, array $data = null){
-      if($data == null) return true;
-      if(empty($data[1]) or empty($data[2])){
-        $this->openMuteByNameForm($player, true);
-      }
-      if(!file_exists($this->plugin->getServer()->getDataPath() . "players/" . strtolower($data[1]) . ".dat")){
-        $player->sendMessage(Main::ERROR . "Target player does not exist");
-      }
-      $target = $this->plugin->getServer()->getPlayerExact($data[1]);
-      if($data[1] == $player->getName()){
-        $player->sendMessage(Main::ERROR . "You can not mute your self");
-      }
-      if($target instanceof Player){
-        if($target->hasPermission("muteassistant.command.mute") or $target->isOp()){
-          $player->sendMessage(Main::ERROR . "You can not mute server staff");
-        } else {
-          if($data[3]){
-            $this->plugin->mute($player, $target, $data[2], $data[4], $data[5], $data[6]);
-          } else {
-            $this->plugin->mute($player, $target, $data[2], "N/A", "N/A", "N/A");
-          }
+    public function handleResponse(Player $player, $data): void {
+        // Form response handling
+    }
+
+    public function jsonSerialize(): array {
+        return $this->data;
+    }
+
+    public function resort(array $array, bool $type = false): array {
+        $a = $array;
+        arsort($a);
+        if($type) return array_reverse($a);
+        return array_reverse(array_values($a));
+    }
+
+    public function openErrorForm(Player $player, string $title = "Error", string $content = "Something went wrong!", array $buttons = []): void {
+        $this->data = [];
+        $this->data["type"] = "form";
+        $this->data["title"] = $title;
+        $this->data["content"] = $content . str_repeat("\n", 15);
+        $this->data["buttons"] = $buttons;
+        $player->sendForm($this);
+    }
+
+    public function openMainForm(Player $player): void {
+        $form = new SF(function (Player $player, $data = null){
+            if($data === null) return;
+            switch($data){
+                case 0:
+                    $this->openMuteForm($player);
+                    break;
+                case 1:
+                    $this->openMuteByNameForm($player);
+                    break;
+                case 2:
+                    $this->openMuteListForm($player);
+                    break;
+            }
+        });
+        $form->setTitle("Mute Assistant Menu");
+        $form->setContent("Choose an option:");
+        $form->addButton("Mute");
+        $form->addButton("Mute By Name");
+        $form->addButton("Mute List");
+        $player->sendForm($form);
+    }
+
+    public function openMuteForm(Player $player): void {
+        $this->l = [];
+        foreach($this->plugin->getServer()->getOnlinePlayers() as $p){
+            $this->l[] = $p->getName();
         }
-      } else {
-        $player->sendMessage(TF::RED . "Player not found!");
-      }
-    });
-    switch($type){
-      case true:
-        $form->addLabel(TF::RED . "Inputs can not be empty!\n\n");
-      break;
-      case false:
+        $form = new CF(function (Player $player, ?array $data){
+            if($data === null) return;
+            $a = $this->resort($this->l);
+            $target = $this->plugin->getServer()->getPlayerExact($a[$data[1]]);
+            if($a[$data[1]] === $player->getName()){
+                $player->sendMessage(Main::ERROR . "You cannot mute yourself");
+                return;
+            }
+            if($target instanceof Player){
+                if($target->hasPermission("muteassistant.command.mute") || $target->hasPermission("pocketmine.group.operator")){
+                    $player->sendMessage(Main::ERROR . "You cannot mute server staff");
+                } else {
+                    if($data[3]){
+                        $this->plugin->mute($player, $target, $data[2], $data[4], $data[5], $data[6]);
+                    } else {
+                        $this->plugin->mute($player, $target, $data[2], "N/A", "N/A", "N/A");
+                    }
+                }
+            } else {
+                $player->sendMessage(TF::RED . "Player not found!");
+            }
+            $this->l = [];
+        });
+        $form->setTitle("Mute Form");
         $form->addLabel("Fill the form and submit to mute your target player:\n\n");
-      break;
+        $form->addDropdown("Target:", $this->resort($this->l));
+        $form->addInput("Reason:", "type here...");
+        $form->addToggle("Permanent:", false);
+        $form->addSlider("Day(s)", 0, 30, 1, 0);
+        $form->addSlider("Hour(s)", 0, 24, 1, 0);
+        $form->addSlider("Minute(s)", 0, 60, 2, 2);
+        $player->sendForm($form);
     }
-    $form->setTitle("Mute Form");
-    $form->addInput("Target:", "type here...");
-    $form->addInput("Reason:", "type here...");
-    $form->addToggle("Prement:", false);
-    $form->addSlider("Day(s)", 0, 30, 1, 0);
-    $form->addSlider("Hour(s)", 0, 24, 1, 0);
-    $form->addSlider("Minutes(s)", 0, 60, 2, 2);
-    $form->sendToPlayer($player);
-    return $form;
-  }
 
-  public function openMuteListForm(Player $player){
-    if(isset($this->target[$player->getName()])) unset($this->target[$player->getName()]);
-    $form = new SF(function (Player $player, $data = null){
-      if($data === null) return true;
-      $this->target[$player->getName()] = $data;
-      $this->openMuteInfoForm($player);
-    });
-    $form->setContent("Choose a player from list to see it's info:");
-    $db = $this->plugin->db->query("SELECT * FROM mutelist;");
-    $list = $db->fetchArray(SQLITE3_ASSOC);
-    if(empty($list)){
-      $this->openErrorForm($player, "Mute List", "Mute list is empty");
-      return true;
+    public function openMuteByNameForm(Player $player, bool $type = false): void {
+        $form = new CF(function (Player $player, ?array $data){
+            if($data === null) return;
+            if(empty($data[1]) || empty($data[2])){
+                $this->openMuteByNameForm($player, true);
+                return;
+            }
+            if(!file_exists($this->plugin->getServer()->getDataPath() . "players/" . strtolower($data[1]) . ".dat")){
+                $player->sendMessage(Main::ERROR . "Target player does not exist");
+                return;
+            }
+            $target = $this->plugin->getServer()->getPlayerExact($data[1]);
+            if($data[1] == $player->getName()){
+                $player->sendMessage(Main::ERROR . "You cannot mute yourself");
+                return;
+            }
+            if($target instanceof Player){
+                if($target->hasPermission("muteassistant.command.mute") || $target->hasPermission("pocketmine.group.operator")){
+                    $player->sendMessage(Main::ERROR . "You cannot mute server staff");
+                } else {
+                    if($data[3]){
+                        $this->plugin->mute($player, $target, $data[2], $data[4], $data[5], $data[6]);
+                    } else {
+                        $this->plugin->mute($player, $target, $data[2], "N/A", "N/A", "N/A");
+                    }
+                }
+            } else {
+                $player->sendMessage(TF::RED . "Player not found!");
+            }
+        });
+        $form->setTitle("Mute Form");
+        if($type){
+            $form->addLabel(TF::RED . "Inputs cannot be empty!\n\n");
+        } else {
+            $form->addLabel("Fill the form and submit to mute your target player:\n\n");
+        }
+        $form->addInput("Target:", "type here...");
+        $form->addInput("Reason:", "type here...");
+        $form->addToggle("Permanent:", false);
+        $form->addSlider("Day(s)", 0, 30, 1, 0);
+        $form->addSlider("Hour(s)", 0, 24, 1, 0);
+        $form->addSlider("Minute(s)", 0, 60, 2, 2);
+        $player->sendForm($form);
     }
-    $db = $this->plugin->db->query("SELECT * FROM mutelist;");
-    $i = -1;
-    while($r = $db->fetchArray(SQLITE3_ASSOC)){
-      $j = $i + 1;
-      $mp = $r["player"];
-      $form->addButton($mp, -1, "", $mp);
-      $i = $i + 1;
-    }
-    $form->sendToPlayer($player);
-    return $form;
-  }
 
-  public function openMuteInfoForm(Player $player){
-    $form = new SF(function (Player $player, $data = null){
-      $result = $data;
-      if($result === null) return true;
-      switch($result){
-        case 0:
-          $tpn = $this->target[$player->getName()];
-          $db = $this->plugin->db->query("SELECT * FROM mutelist WHERE player = '$tpn';");
-          $r = $db->fetchArray(SQLITE3_ASSOC);
-          if(!empty($r)){
-            $this->plugin->db->query("DELETE FROM mutelist WHERE player = '$tpn';");
-            $player->sendMessage(Main::INFO . $tpn . " unmuted!");
-          } else {
+    public function openMuteListForm(Player $player): void {
+        if(isset($this->target[$player->getName()])) unset($this->target[$player->getName()]);
+        $form = new SF(function (Player $player, $data = null){
+            if($data === null) return;
+            $this->target[$player->getName()] = $data;
+            $this->openMuteInfoForm($player);
+        });
+        $form->setTitle("Mute List");
+        $form->setContent("Choose a player from list to see their info:");
+        $db = $this->plugin->db->query("SELECT * FROM mutelist;");
+        $list = $db->fetchArray(SQLITE3_ASSOC);
+        if(empty($list)){
+            $this->openErrorForm($player, "Mute List", "Mute list is empty");
+            return;
+        }
+        $db = $this->plugin->db->query("SELECT * FROM mutelist;");
+        while($r = $db->fetchArray(SQLITE3_ASSOC)){
+            $mp = $r["player"];
+            $form->addButton($mp, -1, "", $mp);
+        }
+        $player->sendForm($form);
+
+    }
+
+    public function openMuteInfoForm(Player $player): void {
+        $form = new SF(function (Player $player, $data = null){
+            if($data === null) return;
+            if($data === 0){
+                $tpn = $this->target[$player->getName()];
+                $db = $this->plugin->db->query("SELECT * FROM mutelist WHERE player = '$tpn';");
+                $r = $db->fetchArray(SQLITE3_ASSOC);
+                if(!empty($r)){
+                    $this->plugin->db->query("DELETE FROM mutelist WHERE player = '$tpn';");
+                    $player->sendMessage(Main::INFO . $tpn . " unmuted!");
+                } else {
+                    $player->sendMessage(Main::ERROR . "Something went wrong!");
+                }
+                unset($this->target[$player->getName()]);
+            }
+        });
+        $form->setTitle($this->target[$player->getName()] . "'s Mute Info");
+        $t = $this->target[$player->getName()];
+        $db = $this->plugin->db->query("SELECT * FROM mutelist WHERE player = '$t';");
+        $data = $db->fetchArray(SQLITE3_ASSOC);
+        if(!empty($data)){
+            if($data["time"] < 0){
+                $form->setContent("Days left: N/A\nHours left: N/A\nMinutes left: N/A\nSeconds: N/A\nMuted by " . TF::GREEN . $data["staff"] . TF::WHITE . " for the reason:\n" . $data["reason"] . str_repeat("\n", 8));
+            } else {
+                $rt = $data["time"] - time();
+                $d = floor($rt / 86400);
+                $hs = $rt % 86400;
+                $h = floor($hs / 3600);
+                $ms = $hs % 3600;
+                $m = floor($ms / 60);
+                $rs = $ms % 60;
+                $s = ceil($rs);
+                $form->setContent("Days left: " . $d . "\nHours left: " . $h . "\nMinutes left: " . $m . "\nSeconds: " . $s . "\nMuted by " . TF::GREEN . $data["staff"] . TF::WHITE . " for the reason:\n" . $data["reason"] . str_repeat("\n", 8));
+            }
+            $form->addButton("UNMUTE");
+        } else {
             $player->sendMessage(Main::ERROR . "Something went wrong!");
-          }
-          unset($this->target[$player->getName()]);
-        break;
-      }
-    });
-    $form->setTitle($this->target[$player->getName()] . "'s Mute Info");
-    $t = $this->target[$player->getName()];
-    $db = $this->plugin->db->query("SELECT * FROM mutelist WHERE player = '$t';");
-    $data = $db->fetchArray(SQLITE3_ASSOC);
-    if(!empty($data)){
-      if($data["time"] < 0){
-        $form->setContent("Days left: N/A\nHours left: N/A\nMinutes left: N/A\nSeconds: N/A\nMuted by " . TF::GREEN . $data["staff"] . TF::WHITE . " for the reason:\n" . $data["reason"] . str_repeat("\n", 8));
-      } else {
-        $rt = $data["time"] - time();
-        $d = floor($rt / 86400);
-        $hs = $rt % 86400;
-        $h = floor($hs / 3600);
-        $ms = $hs % 3600;
-        $m = floor($ms / 60);
-        $rs = $ms % 60;
-        $s = ceil($rs);
-        $form->setContent("Days left: " . $d . "\nHours left: " . $h . "\nMinutes left: " . $m . "\nSeconds: " . $s . "\nMuted by " . TF::GREEN . $data["staff"] . TF::WHITE . " for the reason:\n" . $data["reason"] . str_repeat("\n", 8));
-      }
-      $form->addButton("UNMUTE");
-    } else {
-      $player->sendMessage(Main::ERROR . "Something went wrong!");
-    }
-    $form->sendToPlayer($player);
-    return $form;
-  }
+        }
+        $player->sendForm($form);
 
+    }
 }
